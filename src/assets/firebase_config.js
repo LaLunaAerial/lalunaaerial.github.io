b22cabee30e690ecefbb3d802b122962a7fb79d7
LaLunaAerial <la.luna.aerial@gmail.com>
2025-07-07 14:30:44 +0800

    fixed the Home icon and the Home picture

---
+++ src/assets/firebase_config.js
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAQL2r9vwcSwmgNv-70jnogSJGGJ2cHzSM",
+  authDomain: "laluna-website.firebaseapp.com",
+  databaseURL: "https://laluna-website-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "laluna-website",
+  storageBucket: "laluna-website.firebasestorage.app",
+  messagingSenderId: "1010305782926",
+  appId: "1:1010305782926:web:f13068fb853892469865df",
+  measurementId: "G-ETL2YPFD24"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
