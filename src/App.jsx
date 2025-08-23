c6b4d7f26370e96af702c776f95bb92462bb2019
alexkkm <alexkong0222@gmail.com>
2025-08-23 12:38:15 +0800

    fixing the Navbar covered by content issue

--- src/App.jsx
+++ src/App.jsx
@@ -27,13 +27,13 @@ function New() {
     return (
     <BrowserRouter>
       <div>
-        <button onClick={handleToggleNavbar} style={{ position: 'fixed', top: 0, right: 0, zIndex: 1 }}>
+        <button onClick={handleToggleNavbar} style={{ position: 'fixed', top: 0, right: 0}}>
           {(showNavbar)?"X":"Menu"}
         </button>
         {showNavbar && (
         <Navbar style={{ position: 'fixed', top: 0, right: 0, height: '100vh', width: '300px' }} onLinkClick={handleLinkClick} />
         )}
-        <div style={{ marginLeft: showNavbar ? '0' : '0', marginRight: showNavbar ? '300px' : '0', filter: showNavbar ? 'blur(8px)' : 'none', pointerEvents: showNavbar ? 'none' : 'auto' }}>
+        <div style={{marginLeft: showNavbar ? '0' : '0', marginRight: showNavbar ? '400px' : '0', filter: showNavbar ? 'blur(8px)' : 'none', pointerEvents: showNavbar ? 'none' : 'auto' }}>
           <Routes>
             <Route path='/' element={<HomePage />} />
             <Route path='/login' element={<LoginPage />} />
